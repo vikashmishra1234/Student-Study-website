006233f52cd4dd41a6487dcd593f67f3c14ff7cd
vikash mishra <vikashmishra8371@gmail.com>
2024-09-13 21:33:52 +0530

    issue

--- Student/src/components/Api.js
+++ Student/src/components/Api.js
@@ -3,7 +3,7 @@ import Cookies from 'js-cookie';
 import Swal from 'sweetalert2'; // Import SweetAlert2
 
 // const API_BASE_URL = 'http://localhost:5000'; // Update with your backend URL
-const API_BASE_URL = 'https://student-study-backend.onrender.com/'; // Update with your backend URL
+const API_BASE_URL = 'https://student-study-backend.onrender.com'; // Update with your backend URL
 
 // Function to display error using SweetAlert2
 const showErrorAlert = (message) => {
